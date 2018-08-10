8b14ecb275fa5e82e96068f9b4db8dc25e90cd3a
Ignacio Frund <GeneralCondon@users.noreply.github.com>
2018-08-10 10:51:09 -0300

    Nivel 1
    te gusta ahora tomas la re concha de tu hermana

--- Frund/Tiled/Template.tsx
+++ Frund/Tiled/Template.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="Template" tilewidth="16" tileheight="16" tilecount="10000" columns="100">
- <image source="../GitHub/Tony/Gráficos/Terminados/Template.png" width="1600" height="1600"/>
+ <image source="../GitHub/Tony/Frund/Terminados/Template.png" width="1600" height="1600"/>
 </tileset>
