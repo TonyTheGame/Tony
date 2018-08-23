bad78e1695317636e0ac3cbd8aba1684e5674f3d
Ignacio Frund <39309268+GeneralCondon@users.noreply.github.com>
2018-08-23 14:49:58 -0300

    Graficos 17/8

---
+++ Frund/Tiled/TemplateCasas1LadrillosVerdes.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="TemplateCasas1LadrillosVerdes" tilewidth="16" tileheight="16" tilecount="992" columns="32">
+ <image source="../Terminados/TemplateCasas1LadrillosVerdes.png" width="512" height="496"/>
+</tileset>
---
+++ Frund/Tiled/Tileset Nivel 2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Tileset Nivel 2" tilewidth="16" tileheight="16" tilecount="10000" columns="37">
+ <image source="../Terminados/TemplateNivel222.png" width="592" height="592"/>
+</tileset>
