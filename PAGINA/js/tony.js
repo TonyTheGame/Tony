c629a23d95e6a776633ff82663576002d93bf912
Tony <carloscarlito4594@gmail.com>
2018-08-24 08:11:55 -0300

    kk
    kk

--- PAGINA/js/tony.js
+++ PAGINA/js/tony.js
@@ -13,3 +13,20 @@ $(window).resize(function() {
 	$parrafo.css('margin-left', (($parrafo.width() / 2) * -1) + "px");
 	$parrafo.css('margin-top', (($parrafo.height() / 2) * -1) + "px");
 });
+
+
+$(document).ready(function() {
+	var $imagen = $("#titulo");	
+
+	$imagen.css('margin-left', (($imagen.width() / 2) * -1) + "px");
+	$imagen.css('margin-top', (($imagen.height() / 2) * -1) + "px");
+
+	$("body").vide('video.mp4');
+});
+
+$(window).resize(function() {
+	var $imagen = $("#titulo");	
+
+	$imagen.css('margin-left', (($imagen.width() / 2) * -1) + "px");
+	$imagen.css('margin-top', (($imagen.height() / 2) * -1) + "px");
+});
