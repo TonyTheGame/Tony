4ecf49322624e788a97def51493728f07e949340
Ignacio Frund <GeneralCondon@users.noreply.github.com>
2018-08-24 12:14:48 -0300

    NIVEL 3

---
+++ Frund/Tiled/TemplateCasas1Noche.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="TemplateCasas1Noche" tilewidth="16" tileheight="16" tilecount="992" columns="32">
+ <image source="../Terminados/TemplateCasas1Noche.png" width="512" height="496"/>
+</tileset>
