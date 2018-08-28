3e57e19cf5000f73cb90b3c3c862e99b11948b92
Tony <carloscarlito4594@gmail.com>
2018-08-28 13:50:16 -0300

    28-08
    eliminar los parrafos de las imagenes y que la imagen tenga la imagen y el texto

--- PAGINA/js/tony.js
+++ PAGINA/js/tony.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
 	var $parrafo = $("#resumen");	
 
 	$parrafo.css('margin-left', (($parrafo.width() / 2) * -1) + "px");
@@ -29,4 +29,8 @@ $(window).resize(function() {
 
 	$imagen.css('margin-left', (($imagen.width() / 2) * -1) + "px");
 	$imagen.css('margin-top', (($imagen.height() / 2) * -1) + "px");
+});*/
+
+$(document).ready(function() {
+	$("body").vide('video.mp4');
 });
