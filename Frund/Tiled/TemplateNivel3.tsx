1c87bc0d67d7e464b38875b6852cd35dab422fbb
Ignacio Frund <GeneralCondon@users.noreply.github.com>
2018-09-07 10:56:31 -0300

    07-09

---
+++ Frund/Tiled/TemplateNivel3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="TemplateNivel3" tilewidth="16" tileheight="16" tilecount="360" columns="30">
+ <image source="../Terminados/TemplateNivel3.png" width="480" height="192"/>
+</tileset>
--- Frund/Tiled/TemplateNoche.tsx
+++ Frund/Tiled/TemplateNoche.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="TemplateNoche" tilewidth="16" tileheight="16" tilecount="10000" columns="100">
- <image source="../Terminados/TemplateNoche.png" width="1600" height="1600"/>
+ <image source="../Terminados/Template.png" width="1600" height="1600"/>
 </tileset>
