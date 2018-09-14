c49279370a95b3220bb85ee1258cbdbe2718ab94
Ignacio Frund <GeneralCondon@users.noreply.github.com>
2018-09-14 13:24:39 -0300

    la concha de tu hermana tomas 14-09

--- Frund/Tiled/Tileset Nivel 2.tsx
+++ Frund/Tiled/Tileset Nivel 2.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="Tileset Nivel 2" tilewidth="16" tileheight="16" tilecount="1369" columns="37">
- <image source="../Terminados/TemplateNivel22.png" width="592" height="592"/>
+ <image source="../Terminados/TemplateNivel2.png" width="592" height="592"/>
 </tileset>
