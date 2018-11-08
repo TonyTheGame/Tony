65143272a2bea706e8d1023cb2a3151b5ea1de8c
Ignacio Frund <39309268+GeneralCondon@users.noreply.github.com>
2018-11-08 19:00:48 -0300

    11-8

--- PAGINA/js/tony.js
+++ PAGINA/js/tony.js
@@ -34,3 +34,15 @@ $(window).resize(function() {
 $(document).ready(function() {
 	$("body").vide('video.mp4');
 });
+
+/*window.onscroll = function() {myFunction()};
+var header = document.getElementById("header");
+var sticky = header.offsetTop;
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    header.classList.add("sticky")
+  } else {
+    header.classList.remove("sticky");
+  }
+} 
+*/
